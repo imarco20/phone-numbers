fd24c260f52e9481ccf2ff79c5d250e1dd11cddf
imarco20 <marcode420@gmail.com>
2021-09-21 22:34:37 +0200

    add database connection to the application

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"fmt"
+	_ "github.com/mattn/go-sqlite3"
 	"log"
 	"net/http"
 	"os"
@@ -11,6 +13,7 @@ import (
 
 type config struct {
 	port int
+	dsn  string
 }
 
 type application struct {
@@ -22,9 +25,22 @@ func main() {
 	var cfg config
 
 	flag.IntVar(&cfg.port, "port", 8080, "API server port")
+	flag.StringVar(&cfg.dsn, "dsn", "sample.db", "sqlite data source name")
 	flag.Parse()
 
 	logger := log.New(os.Stdout, "", log.LstdFlags)
+
+	db, err := openDB(cfg.dsn)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	defer func() {
+		err := db.Close()
+		if err != nil {
+			logger.Fatal(err)
+		}
+	}()
 
 	app := &application{
 		config: cfg,
@@ -41,8 +57,21 @@ func main() {
 
 	logger.Printf("starting server on %s", server.Addr)
 
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	if err != nil {
 		logger.Fatal(err)
 	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, err
 }
