4edc08ff265bec5765713d965be2880aa77d8f98
imarco20 <marcode420@gmail.com>
2021-09-22 22:05:23 +0200

    add middleware for CORS

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,12 +1,22 @@
 package main
 
-import "net/http"
+import (
+	"github.com/rs/cors"
+	"net/http"
+)
 
 func (app *application) routes() http.Handler {
 	router := http.NewServeMux()
 
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:4200"},
+		AllowedMethods:   []string{"POST", "GET", "OPTIONS", "PUT", "DELETE"},
+		AllowedHeaders:   []string{"*"},
+		AllowCredentials: true,
+	})
+
 	router.Handle("/api", http.HandlerFunc(app.filterHandler))
 	router.Handle("/api/health", http.HandlerFunc(app.healthCheckHandler))
 
-	return router
+	return c.Handler(router)
 }
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.16
 require (
 	github.com/google/go-cmp v0.5.6
 	github.com/mattn/go-sqlite3 v1.14.8
+	github.com/rs/cors v1.8.0
 )
